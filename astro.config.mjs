ec9eb69aaeeffdba213037c656183781fce1a766
corrinag <corrinagarton@linosystems.com>
2025-09-29 15:54:12 -1000

    Update astro.config to use SITE_URL and adjust .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  //site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: process.env.SITE_URL || "http://localhost:3000",
+
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   
